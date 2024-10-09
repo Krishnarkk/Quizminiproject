c29e7abe57049652eeb2a105045957e0e404b0d8
Krishnajireact <86544748+Krishnarkk@users.noreply.github.com>
2024-10-09 12:28:21 +0530

    added loader to add questions

--- quizapp/src/components/Navbar/AddQuestions.jsx
+++ quizapp/src/components/Navbar/AddQuestions.jsx
@@ -1,94 +1,103 @@
 import React, { useContext, useState } from "react";
 import { QuestionContext } from "./QuestionContext";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Loader from "./Loader";
+
 const AddQuestions = () => {
   const { loggedInUser, addNewQuestion } = useContext(QuestionContext);
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("JavaScript");
   const [otherCategory, setOtherCategory] = useState("");
-  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  console.log(otherCategory);
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
     const updatedCategory = category === "Other" ? otherCategory : category;
     const newQuestion = {
       id: Date.now(),
       title,
       category: updatedCategory,
-      name,
       answers: [],
-      postedBy:loggedInUser?.username
+      postedBy: loggedInUser?.username,
     };
-    addNewQuestion(newQuestion);
-    navigate("/");
+
+    setTimeout(() => {
+      addNewQuestion(newQuestion);
+      setLoading(false);
+      navigate("/");
+    }, 1000);
   };
 
-  const handleCategory = (e) => {
+  const handleCategoryChange = (e) => {
     setCategory(e.target.value);
   };
+
   return (
-    <form onSubmit={handleSubmit} className="mb-4 mt-4 container">
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Enter your Question
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Enter your name
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="name"
-          value={loggedInUser?.username}
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="category" className="form-label">
-          Category of question
-        </label>
-        <select
-          id="category"
-          className="form-select"
-          value={category}
-          onChange={(e) => handleCategory(e)}
+    <div className="container">
+      {loading && <Loader />}
+      <form onSubmit={handleSubmit} className="mb-4 mt-4">
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Enter your Question
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Posted By
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={loggedInUser?.username}
+            readOnly
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Category of question
+          </label>
+          <select
+            id="category"
+            className="form-select"
+            value={category}
+            onChange={handleCategoryChange}
+          >
+            <option value="JavaScript">JavaScript</option>
+            <option value="HTML">HTML</option>
+            <option value="CSS">CSS</option>
+            <option value="ReactJs">ReactJs</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        {category === "Other" && (
+          <input
+            type="text"
+            className="form-control"
+            value={otherCategory}
+            onChange={(e) => setOtherCategory(e.target.value)}
+            placeholder="Enter your question category"
+          />
+        )}
+        <button
+          type="submit"
+          className="btn btn-danger mt-4"
+          disabled={loading}
         >
-          <option value="JavaScript">JavaScript</option>
-          <option value="HTML">HTML</option>
-          <option value="CSS">CSS</option>
-          <option value="ReactJs">ReactJs</option>
-          <option value="Other">Other</option>
-        </select>
-      </div>
-      {category === "Other" && (
-        <input
-          type="text"
-          className="form-control"
-          value={otherCategory}
-          onChange={(e) => {
-            setOtherCategory(e.target.value);
-          }}
-          placeholder="enter your question category"
-        />
-      )}
-      <button type="submit" className="btn btn-danger mt-4">
-        Post Question
-      </button>
-    </form>
+          {loading ? "Posting..." : "Post Question"}
+        </button>
+      </form>
+    </div>
   );
 };
 
